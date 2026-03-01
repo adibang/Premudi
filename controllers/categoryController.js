e87e48a00a1b1e71727fe0e81c556796e6afdb51
adibang <adiblinggo@gmail.com>
2026-03-01 15:43:23 +0700

    Create categoryController.js

---
+++ controllers/categoryController.js
@@ -0,0 +1,41 @@
+const Category = require('../models/Category');
+
+exports.getAll = async (req, res) => {
+  try {
+    const categories = await Category.find().sort({ name: 1 });
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = new Category({ name });
+    await category.save();
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const category = await Category.findByIdAndUpdate(id, { name }, { new: true });
+    res.json(category);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    await Category.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
